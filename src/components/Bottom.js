055091fec4fbf587634bd5334f6219accf8ad0b4
Zach Hoskins <zthoskins@comhs.org>
2022-06-28 16:32:03 -0500

    Updates

--- src/components/Bottom.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Bottom() {
-  return (
-    <footer></footer>
-  )
-}
-
-export default Bottom
--- src/components/ContentWithRightRail.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ContentWithRightRail({children}) {
-  return (
-    <div className="main-right-rail">{children}</div>
-  )
-}
-
-export default ContentWithRightRail
