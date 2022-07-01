cfaf1a309233e3a72120196f551471dc04d0abc5
Zach Hoskins <zach@zachos.tech>
2022-07-01 09:54:43 -0500

    fixed gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,34 @@ const dom = require('gulp-dom');
 const inlinesource = require('gulp-inline-source');
 const replace = require('gulp-replace');
 
-gulp.task('default', () => {
+function prebundle() {
+    return gulp.src('./package.json')
+        .pipe(replace('\"homepage\"', '\"homepage_DelToDeploy_\"'))
+        .pipe(gulp.dest('.'));
+}
+
+function bundle() {
     return gulp.src('./build/*.html')
         .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
         .pipe(replace('defer="defer" ', 'type="text/javascript" data-id="script" '))
         .pipe(replace('.js"></script>', '.js" inline></script>'))
-        .pipe(dom(function() {
+        .pipe(replace())
+        .pipe(dom(function () {
             this.body.appendChild(this.querySelector('[data-id="script"]'))
         }))
         .pipe(replace('data-id="script" ', ''))
         .pipe(inlinesource({
             compress: false,
         }))
-        .pipe(gulp.dest('./build/bundled'))
-});
+        .pipe(gulp.dest('./build/bundle'))
+}
+
+function cleanup() {
+    return gulp.src('./package.json')
+        .pipe(replace('\"homepage_DelToDeploy_\"', '\"homepage\"'))
+        .pipe(gulp.dest('.'));
+}
+
+exports.prebundle = prebundle;
+
+exports.default = gulp.series(bundle,cleanup);
