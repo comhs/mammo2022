5b587c7cc5043cb40b78871056efb732e52740ae
Zach Hoskins <zach@zachos.tech>
2022-07-13 16:53:57 -0500

    Added rotating hero image, and attempted to fix sizing issue with headers on sm desktops

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,25 @@
-import React from 'react';
-// import heroImage from '../images/hero.png'
+import React, { useEffect, useState } from 'react';
+import '../styles/Hero.css';
+const json = require('../api/images.json');
 
 function Hero({ children, phone, darkText }) {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImgIndex((imgIndex + 1) % json.images.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  })
+
   return (
     <div className="hero">
-      <div className="hero-image"><img src="https://raw.githubusercontent.com/comhs/mammo2022/main/src/images/hero.png" alt="Hero" /></div>
-      {/* <div className="hero-image"><img src={heroImage} alt="Hero" /></div> */}
+      <div className="hero-image">
+        <img key={json.images[imgIndex].key} src={json.images[imgIndex].url} alt={json.images[imgIndex].alt} />
+      </div>
       <div className="hero-cta">
         <h1 className={darkText === "true" ? "headline-dark" : "headline"}>{children}</h1>
         <a href={"tel:" + phone.replace(/[^0-9]/g, "")} className="hero-phone">{phone}</a>
-        {/* <a href={"tel:" + phone.replace(/[^0-9*]/g, "")} className="hero-phone">{phone}</a> */}
         {/* <div className="hero-button" id="mainBtn">Request&nbsp;an&nbsp;Appointment</div> */}
       </div>
     </div>
