8b9d2e934e6edf9e3b7d47bfc27d7fdf74b8f3c0
Zach Hoskins <zach@zachos.tech>
2022-08-02 15:51:15 -0500

    Using final image location

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Header() {
   return (
     <header>
-      <img src="https://www.comhs.org/~/media/Images/Community-Healthcare-Only/Campaigns/CV/Cardiology-Destination/LOGO.png" alt="CHS Logo" class="logo" />
-      <img src="https://www.comhs.org/~/media/Images/Community-Healthcare-Only/Campaigns/CV/Cardiology-Destination/DestinationHealth.png" alt="Destination Health" class="destination-health" />
+      <img src="https://www.comhs.org/~/media/Images/Community-Healthcare-Only/Campaigns/CHSLogo.png" alt="CHS Logo" class="logo" />
+      <img src="https://www.comhs.org/~/media/Images/Community-Healthcare-Only/Campaigns/DestinationHealth.png" alt="Destination Health" class="destination-health" />
     </header>
   )
 }
