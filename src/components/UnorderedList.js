796af9a72b10ac64268c39c074ef821ca4179aaa
Zach Hoskins <zach@zachos.tech>
2022-08-03 14:34:45 -0500

    Fixed typo

--- src/components/UnorderedList.js
+++ src/components/UnorderedList.js
@@ -3,7 +3,7 @@ import React from 'react'
 const UnorderedList = () => {
   return (
     <ul>
-      <li>A mammogram is one of the simplest and most effective ways to save lives. The test can detect cancer in its earliest stage when it is often the most treatable</li>
+      <li>A mammogram is one of the simplest and most effective ways to save lives. The test can detect cancer in its earliest stage when it is often the most treatable.</li>
       <li>It’s easy and convenient to book your mammogram at any of six Community Healthcare System locations. With one call, we’ll find a place and time that’s convenient for you.</li>
     </ul>
   )
